52d94683dcec208e9c320999ef81f95e201210da
Mark-Mitchell <marklukemitchell6@gmail.com>
2022-05-02 16:57:08 +0200

    added a rock that can be picked up

---
+++ Objects/rock.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var in_area = false
+var has_rock = false
+
+
+func _on_Area2D_area_entered(area):
+	if area.name == "CheckForRock":
+		in_area = true
+#	print(area.name)
+
+func _on_Area2D_area_exited(area):
+	if area.name == "CheckForRock":
+		in_area = false
+		
+func _physics_process(delta):
+	if Input.is_action_just_pressed("use_item"):
+		if (in_area):
+			has_rock = true
+			queue_free()
+		else:
+			has_rock = false
